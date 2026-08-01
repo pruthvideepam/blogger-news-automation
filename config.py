b8de25dc05b10e19bba95fbf5265c32b26a0a413
Pruthvi Deepam L A <157585780+pruthvideepam@users.noreply.github.com>
2026-08-01 17:14:52 +0530

    Refactor RSS_FEEDS structure in config.py

--- config.py
+++ config.py
@@ -1,12 +1,213 @@
 BLOG_ID = "1358510809725283320"
 
-RSS_FEEDS = [    
-{
+RSS_FEEDS = [
+    {
         "name": "Prajavani",
         "url": "https://www.prajavani.net/stories.rss?section=karnataka-news",
         "labels": ["States", "Karnataka"]
     },
-{
+    {
+        "name": "Prajavani Udupi",
+        "url": "https://www.prajavani.net/stories.rss?section=udupi",
+        "labels": ["States", "Karnataka", "Udupi"]
+    },
+    {
+        "name": "Prajavani Kalaburagi",
+        "url": "https://www.prajavani.net/stories.rss?section=kalaburagi",
+        "labels": ["States", "Karnataka", "Kalaburagi"]
+    },
+    {
+        "name": "Prajavani Kodagu",
+        "url": "https://www.prajavani.net/stories.rss?section=kodagu",
+        "labels": ["States", "Karnataka", "Kodagu"]
+    },
+    {
+        "name": "Prajavani Kolar",
+        "url": "https://www.prajavani.net/stories.rss?section=kolar",
+        "labels": ["States", "Karnataka", "Kolar"]
+    },
+    {
+        "name": "Prajavani Gadaga",
+        "url": "https://www.prajavani.net/stories.rss?section=gadaga",
+        "labels": ["States", "Karnataka", "Gadaga"]
+    },
+    {
+        "name": "Prajavani Chikkaballapur",
+        "url": "https://www.prajavani.net/stories.rss?section=chikkaballapur",
+        "labels": ["States", "Karnataka", "Chikkaballapur"]
+    },
+    {
+        "name": "Prajavani Chikkamagaluru",
+        "url": "https://www.prajavani.net/stories.rss?section=chikkamagaluru",
+        "labels": ["States", "Karnataka", "Chikkamagaluru"]
+    },
+    {
+        "name": "Prajavani Chitradurga",
+        "url": "https://www.prajavani.net/stories.rss?section=chitradurga",
+        "labels": ["States", "Karnataka", "Chitradurga"]
+    },
+    {
+        "name": "Prajavani Dakshina Kannada",
+        "url": "https://www.prajavani.net/stories.rss?section=dakshina-kannada",
+        "labels": ["States", "Karnataka", "Dakshina Kannada"]
+    },
+    {
+        "name": "Prajavani Davanagere",
+        "url": "https://www.prajavani.net/stories.rss?section=davanagere",
+        "labels": ["States", "Karnataka", "Davanagere"]
+    },
+    {
+        "name": "Prajavani Dharwad",
+        "url": "https://www.prajavani.net/stories.rss?section=dharwad",
+        "labels": ["States", "Karnataka", "Dharwad"]
+    },
+    {
+        "name": "Prajavani Ballari",
+        "url": "https://www.prajavani.net/stories.rss?section=ballari",
+        "labels": ["States", "Karnataka", "Ballari"]
+    },
+    {
+        "name": "Prajavani Bagalkot",
+        "url": "https://www.prajavani.net/stories.rss?section=bagalkot",
+        "labels": ["States", "Karnataka", "Bagalkot"]
+    },
+    {
+        "name": "Prajavani Bidar",
+        "url": "https://www.prajavani.net/stories.rss?section=bidar",
+        "labels": ["States", "Karnataka", "Bidar"]
+    },
+    {
+        "name": "Prajavani Uttara Kannada",
+        "url": "https://www.prajavani.net/stories.rss?section=uttara-kannada",
+        "labels": ["States", "Karnataka", "Uttara Kannada"]
+    },
+    {
+        "name": "Prajavani Koppal",
+        "url": "https://www.prajavani.net/stories.rss?section=koppal",
+        "labels": ["States", "Karnataka", "Koppal"]
+    },
+    {
+        "name": "Prajavani Chamarajanagara",
+        "url": "https://www.prajavani.net/stories.rss?section=chamarajanagara",
+        "labels": ["States", "Karnataka", "Chamarajanagara"]
+    },
+    {
+        "name": "Prajavani Tumakuru",
+        "url": "https://www.prajavani.net/stories.rss?section=tumakuru",
+        "labels": ["States", "Karnataka", "Tumakuru"]
+    },
+    {
+        "name": "Prajavani Belagavi",
+        "url": "https://www.prajavani.net/stories.rss?section=belagavi",
+        "labels": ["States", "Karnataka", "Belagavi"]
+    },
+    {
+        "name": "Prajavani Bengaluru City",
+        "url": "https://www.prajavani.net/stories.rss?section=bengaluru-city",
+        "labels": ["States", "Karnataka", "Bengaluru"]
+    },
+    {
+        "name": "Prajavani Bengaluru Rural",
+        "url": "https://www.prajavani.net/stories.rss?section=bangaluru-rural",
+        "labels": ["States", "Karnataka", "Bengaluru Rural"]
+    },
+    {
+        "name": "Prajavani Mysuru",
+        "url": "https://www.prajavani.net/stories.rss?section=mysuru",
+        "labels": ["States", "Karnataka", "Mysuru"]
+    },
+    {
+        "name": "Prajavani Mandya",
+        "url": "https://www.prajavani.net/stories.rss?section=mandya",
+        "labels": ["States", "Karnataka", "Mandya"]
+    },
+    {
+        "name": "Prajavani Yadagiri",
+        "url": "https://www.prajavani.net/stories.rss?section=yadagiri",
+        "labels": ["States", "Karnataka", "Yadagiri"]
+    },
+    {
+        "name": "Prajavani Ramanagara",
+        "url": "https://www.prajavani.net/stories.rss?section=ramanagara",
+        "labels": ["States", "Karnataka", "Ramanagara"]
+    },
+    {
+        "name": "Prajavani Raichur",
+        "url": "https://www.prajavani.net/stories.rss?section=raichur",
+        "labels": ["States", "Karnataka", "Raichur"]
+    },
+    {
+        "name": "Prajavani Vijayanagara",
+        "url": "https://www.prajavani.net/stories.rss?section=vijayanagara",
+        "labels": ["States", "Karnataka", "Vijayanagara"]
+    },
+    {
+        "name": "Prajavani Haveri",
+        "url": "https://www.prajavani.net/stories.rss?section=haveri",
+        "labels": ["States", "Karnataka", "Haveri"]
+    },
+    {
+        "name": "Prajavani Vijayapura",
+        "url": "https://www.prajavani.net/stories.rss?section=vijayapura",
+        "labels": ["States", "Karnataka", "Vijayapura"]
+    },
+    {
+        "name": "Prajavani Hassan",
+        "url": "https://www.prajavani.net/stories.rss?section=hasana",
+        "labels": ["States", "Karnataka", "Hassan"]
+    },
+    {
+        "name": "Prajavani Fact Check",
+        "url": "https://www.prajavani.net/stories.rss?section=fact-check",
+        "labels": ["Fact Check"]
+    },
+    {
+        "name": "Prajavani India",
+        "url": "https://www.prajavani.net/stories.rss?section=india-news",
+        "labels": ["Nation"]
+    },
+    {
+        "name": "Prajavani World",
+        "url": "https://www.prajavani.net/stories.rss?section=world-news",
+        "labels": ["World"]
+    },
+    {
+        "name": "Prajavani Horoscope",
+        "url": "https://www.prajavani.net/stories.rss?section=horoscope",
+        "labels": ["Astrology"]
+    },
+    {
+        "name": "Prajavani Panchanga",
+        "url": "https://www.prajavani.net/stories.rss?section=panchanga",
+        "labels": ["Astrology"]
+    },
+    {
+        "name": "Prajavani Vaastu",
+        "url": "https://www.prajavani.net/stories.rss?section=vaastu",
+        "labels": ["Vaastu"]
+    },
+    {
+        "name": "Prajavani Entertainment",
+        "url": "https://www.prajavani.net/stories.rss?section=entertainment",
+        "labels": ["Entertainment"]
+    },
+    {
+        "name": "Prajavani Sports",
+        "url": "https://www.prajavani.net/stories.rss?section=sports",
+        "labels": ["Sports"]
+    },
+    {
+        "name": "Prajavani Education",
+        "url": "https://www.prajavani.net/stories.rss?section=education",
+        "labels": ["Education"]
+    },
+    {
+        "name": "Prajavani Farming",
+        "url": "https://www.prajavani.net/stories.rss?section=farming",
+        "labels": ["Farming"]
+    },
+
+    {
         "name": "Oneindia Kannada",
         "url": "https://kannada.oneindia.com/rss/feeds/oneindia-kannada-fb.xml",
         "labels": ["States", "Karnataka"]
